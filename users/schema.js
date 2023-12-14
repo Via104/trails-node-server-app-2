8f6ccda8d39bdc55cd35349f23900c5fe7139fa5
Victoria Gomez-Small <gomez-small.v@northeastern.edu>
2023-12-13 22:08:30 -0500

    spelling changed

--- users/schema.js
+++ users/schema.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema(
       enum: ["REGULAR", "ADMIN"],
       default: "REGULAR",
     },
-    favorites: {
+    favourites: {
       type: [Object],
       unique: true,
       required: false,
